ce0384538f732fe844f15b98f4fcb3292a33fb4c
fenlj <knightflj@gmail.com>
2025-04-07 05:31:44 -0700

    alias

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,11 +30,20 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = "wishing"
+            keyPassword = "wishing"
+            storeFile = rootProject.file("wishing.jks")
+            storePassword = "wishing"
+        }
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
